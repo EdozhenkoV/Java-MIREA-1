c83a06d5e05fa0438ec54d429a87c9bd255b036e
Siri_Hate <70503655+SiriHate@users.noreply.github.com>
2020-10-07 12:49:14 +0300

    Added Лабораторная 1
    Added Лабораторная 1

---
+++ Лабораторная 1/src/com/company/GarmonyLine.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GarmonyLine {
+    public void Garmonyline (){
+        for (float i = 1; i < 11; i++) {
+            System.out.println(1 / i);
+        }
+    }
+}
---
+++ Лабораторная 1/src/com/company/TestGarmonyLine.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestGarmonyLine {
+    public static void main(String[] args) {
+        GarmonyLine g1 = new GarmonyLine();
+        g1.Garmonyline();
+    }
+}
